bf6ff5a71d5738412decc0e102939ab0bf6fe921
aleksandree128 <aleksandree128@mail.ru>
2022-07-23 22:30:45 +0500

    mesto-api-full-2

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/loggers');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 app.use(cors);
 mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(bodyParser.json());
